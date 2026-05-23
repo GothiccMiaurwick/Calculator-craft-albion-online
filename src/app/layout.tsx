ef54620e8f5d850cc8ad335c418640258308bd1a
GothiccMiaurwick <sebastiansoler3@gmail.com>
2026-05-23 15:35:55 -0500

    feat: use iconPage3 with alpha

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
   title: "Mochi Craft",
   description: "Advanced crafting and market calculator for Albion Online",
   icons: {
-    icon: "/iconPage.png",
+    icon: "/iconPage3.png",
   },
 };
 
